58ffe7e33d125f548e75a0e71545b469564835d0
JK1320 <kaurjus1@gmail.com>
2022-12-07 12:07:54 +0000

    Add comment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-// -------------arrays-------------
+//                              ----arrays----
 // let numbers: number[] = [1, 2, 3];
 
+//------------------------------------------------------
 
-// tuple: fixed length array with each element having a particular type, 
+//                              ----tuple----
+//fixed length array with each element having a particular type, 
 //most useful when having 2 values like key value pairs
 
 //let user: [number, string] = [1, "Mosh"];
