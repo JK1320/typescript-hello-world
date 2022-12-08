60fcce8eb4c7f30f66a185dbbc2d59857ecfab61
JK1320 <kaurjus1@gmail.com>
2022-12-08 12:54:11 +0000

    Optional chaining

--- src/index.ts
+++ src/index.ts
@@ -108,12 +108,34 @@
 
 //                      ----Nullable types----       
 
-function greet(name: string | null | undefined){
-    if(name)
-    console.log(name.toUpperCase())
-    else console.log('Hell0!');
-};
+// function greet(name: string | null | undefined){
+//     if(name)
+//     console.log(name.toUpperCase())
+//     else console.log('Hell0!');
+// };
 
-greet(undefined);
+// greet(undefined);
 
 //--------------------------------------------------------------------------------------
+
+//                  ----Optional Chaining
+
+type Customer = {
+birthday: Date
+};
+
+function getCustomer(id: number): Customer | null | undefined{
+return id === 0 ? null : { birthday: new Date()};
+}
+
+let customer = getCustomer(0);
+// optional property access operator
+   console.log(customer?.birthday);
+
+// Optional element access operator
+// customer?.[0];
+
+
+// Optional call
+let log: any = null;
+log?.('a');
