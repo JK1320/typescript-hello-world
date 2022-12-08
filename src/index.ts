ce8ee077996c992917425d7c62974d5aa32f0703
JK1320 <kaurjus1@gmail.com>
2022-12-08 11:58:56 +0000

    type Aliases

--- src/index.ts
+++ src/index.ts
@@ -41,12 +41,14 @@
 //---------------------------------------------------------------
 
 //                          ----Objects----
+// Type Aliases 
+type Employee = {
+    readonly id: number,
+     name: string,
+     retire: (date: Date) => void
+}
 
-let employee: {
-   readonly id: number,
-    name: string,
-    retire: (date: Date) => void
-} = {
+let employee: Employee = {
     id: 1, 
     name: 'Mosh',
     retire: (date: Date) => {
