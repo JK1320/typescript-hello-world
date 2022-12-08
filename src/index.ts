9ee1a5b9420e5688ad64b5d4ed394a95e183ed36
JK1320 <kaurjus1@gmail.com>
2022-12-08 11:39:47 +0000

    learned functions

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,23 @@
 
 // PascalCase - camelCase
 
-const enum Size {Small = 1, Medium, Large};
-let mySize: Size = Size.Medium;
-console.log(mySize); 
+// const enum Size {Small = 1, Medium, Large};
+// let mySize: Size = Size.Medium;
+// console.log(mySize); 
+
+//----------------------------------------------------------
+
+//                          ----Functions----
+// enable these 3:  "noUnusedLocals": true, "noUnusedParameters": true,  "noImplicitReturns": true,
+
+
+//function calculateTax(income: number, taxYear = 2022): number{
+    
+//     if (taxYear < 2022)
+//     return income * 1.2;
+//     return income * 1.3;
+// }
+
+// calculateTax(10_000);
+
+//---------------------------------------------------------------
