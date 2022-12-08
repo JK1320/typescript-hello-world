5d07c3b9e050d4192fdefdae9c671afa66ff8927
JK1320 <kaurjus1@gmail.com>
2022-12-08 11:52:27 +0000

    learn Objects

--- src/index.ts
+++ src/index.ts
@@ -39,3 +39,17 @@
 // calculateTax(10_000);
 
 //---------------------------------------------------------------
+
+//                          ----Objects----
+
+let employee: {
+   readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = {
+    id: 1, 
+    name: 'Mosh',
+    retire: (date: Date) => {
+        console.log(date);
+    }
+};
